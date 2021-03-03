595ad12cb57c624289be88a2748244f7ee8cd79c
Bex Fortin <cajunluke@gmail.com>
2021-03-02 17:37:54 -0800

    move styles to sass and copy (#9)

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ import generate from './lib/generate.js';
 import { promisify } from 'util';
 import copy from 'copy';
 
+import sass from 'sass';
+
+async function copyStyles(styleFile, styleDest) {
+  const styles = sass.renderSync({ file: styleFile });
+  
+  await writeFile(path.join(__dirname, styleDest), styles.css.toString(), 'utf8');
+}
+
 async function init() {
   try {
     await mkdir(path.join(__dirname, 'dist'));
@@ -24,7 +32,9 @@ async function init() {
   const indexPage = await generate(srcText.toString(), footerHTML.toString(), indexTemplate.toString(), JSON.parse(infoJson.toString()));
 
   await writeFile(path.join(__dirname, 'dist/index.html'), indexPage, 'utf8');
-
+  
+  copyStyles('src/styles/styles.scss', 'dist/css/styles.css');
+  
   // copy static files
   await promisify(copy)(path.join(__dirname, 'static', '**'), path.join(__dirname, 'dist'));
 }
