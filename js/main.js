778b24b7fb0b7b4db7916ec111b9d48f9cb11bcf
cajunluke <cajunluke@users.noreply.github.com>
2021-09-02 01:48:14 +0000

    deploy: a2eee4d09d2a7ba856689ca3af9b0c69374d9643

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 function findCurrentSection(htmlEl, mainEl) {
   const windowHeight = htmlEl.clientHeight;
-  const scrollHeight = htmlEl.scrollHeight;
-  const scrollTop = htmlEl.scrollTop;
 
-  // figure out which section we're in. basically, in order to make this work well for the first and last sections, we:
-  // 1. pick a point as far down the window as we are down the scrollbar and in the middle of the main element
-  const scrollPct = scrollTop / (scrollHeight - windowHeight);
-  const targetY = (windowHeight - 1) * scrollPct; // targetY is a position but windowHeight is a length, -1 to avoid fencepost error
+  // Figure out which section we're in. We have to take into account both the size of the footnote section and
+  // the position of the page after clicking a link in the sidebar. So we:
+  // 1. Focus on the point 15% of the way down the window (a reasonable place for the reader to be looking at) and
+  // in the middle of the main section
+  
+  // targetY is a position but windowHeight is a length, so -1 to avoid a fencepost error
+  const targetY = (windowHeight - 1) * .15;
+  
   const targetX = (mainEl.offsetLeft + mainEl.scrollWidth) / 2;
 
   // 2. figure out what direct child of <main> that corresponds to
@@ -19,8 +21,8 @@ function findCurrentSection(htmlEl, mainEl) {
     // caught in a margin, don't change the highlight
     return null;
   } else {
-    // above or below main; clamp
-    targetEl = scrollPct < 0.5 ? mainEl.firstElementChild : mainEl.lastElementChild;
+    // above main; just highlight the first possibility
+    targetEl = mainEl.firstElementChild;
   }
 
   // 3. walk back through siblings until we find an h1 or h2
@@ -32,8 +34,21 @@ function findCurrentSection(htmlEl, mainEl) {
 }
 
 function highlightNav(navEl, currentId) {
-  navEl.querySelectorAll('.current').forEach(node => node.classList.remove('current'));
-  navEl.querySelector(`[href="#${currentId}"]`).closest('p, li').classList.add('current');
+  // first remove any previously-selected highlights
+  navEl.querySelectorAll('.currentSection').forEach(node => node.classList.remove('currentSection'));
+  navEl.querySelectorAll('.currentGroup').forEach(node => node.classList.remove('currentGroup'));
+  
+  // find the current section's link in the toc
+  const sectionNode = navEl.querySelector(`[href="#${currentId}"]`).closest('p, li');
+  sectionNode.classList.add('currentSection');
+  
+  // find that section's group's li
+  const groupNode = sectionNode.parentNode.closest('li');
+  groupNode.classList.add('currentGroup');
+  
+  const navScroll = navEl.querySelector('#nav-scroll-container');
+  // scroll the nav panel so the highlight is close to 250px below the top of the screen
+  navScroll.scrollTop = sectionNode.offsetTop - 250;
 }
 
 function main() {
@@ -41,12 +56,17 @@ function main() {
   const navEl = document.getElementsByTagName('nav')[0];
   const mainEl = document.getElementsByTagName('main')[0];
 
-  window.onscroll = function() {
+  const onScroll = () => {
     const currentId = findCurrentSection(htmlEl, mainEl);
     if(currentId) {
       highlightNav(navEl, currentId);
     }
   }
+  
+  window.onscroll = onScroll;
+  
+  // also call the onScroll on page load to highlight the introduction
+  onScroll();
 }
 
 main();
